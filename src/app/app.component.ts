5118be456ff5b865a72aff0e8d666357ce091c0a
Were Samson Bruno <werefast2000@gmail.com>
2023-12-04 19:15:47 +0000

    fixed login component import error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './auth/login/login.component';
+import { LoginComponent } from './auth/login/login.component'
 import { SideNavComponent } from './app-core/common/side-nav/side-nav.component';
 import { TopNavComponent } from './app-core/common/top-nav/top-nav.component';
 import { MaterialModule } from './material/material.module';
@@ -32,7 +32,7 @@ export class AppComponent implements OnChanges {
     console.log(this.name, 'onChanges');
     for (const inputChange in changes){
       console.log(changes[inputChange].firstChange, inputChange);
-    } 
+    }
   }
 
   ngOnInit(): void{
